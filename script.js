31b88605885a6d5732b438a9bfe36997ce49cb89
Vaishaliyadavv <vaishali.yadavv09@gmail.com>
2024-03-29 21:15:26 +0530

    add todosListEl.addEventListener

--- script.js
+++ script.js
@@ -62,11 +62,44 @@ function renderTodos() {
         <i 
         class="bi ${todo.checked ? 'bi-check-circle-fill' : 'bi-circle'}"
         style="color : ${todo.color}"
+        data-action="check"
         ></i>
-        <p class="">${todo.value}</p>
-        <i class="bi bi-pencil-square"></i>
-        <i class="bi bi-trash"></i>
+        <p class="" data-action="check">${todo.value}</p>
+        <i class="bi bi-pencil-square" data-action="edit"></i>
+        <i class="bi bi-trash" data-action="delete"></i>
         
-        </div>`
+        </div>`;
     });
+}
+
+//CLICK EVENT LISTENER FOR ALL THE TODO
+
+todosListEl.addEventListener('click', (event) => {
+    const target = event.target;
+    const parentElement = target.parentNode;
+
+    if (parentElement.className !== 'todo') return;
+
+    // t o d o id
+    const todo = parentElement;
+    const todoId = Number(todo.id);
+
+    // target action
+    const action = target.dataset.action
+
+    action == "check" && checkTodo(todoId);
+    // action == "edit" && editTodo(todoId);
+    // action == "delete" && deleteTodo(todoId);
+
+});
+
+
+function checkTodo(todoId) {
+    todos = todos.map((todo, index) =>
+    ({
+        ...todo,
+        checked: index === todoId ? !todo.checked : todo.checked
+    }));
+
+    renderTodos();
 }
